15f61b95170e0686bbc4c83569d91f346c420bf7
Xia Hua <huaxia1985@gmail.com>
2026-01-23 09:51:30 +1100

    Update model-prosse-trait-lambda-state.R
    updated to newer versions of R

--- R/model-prosse-trait-lambda-state.R
+++ R/model-prosse-trait-lambda-state.R
@@ -296,7 +296,7 @@ make.all.branches.prosse.trait.lambda.state <- function (cache) {
     		}
     		y <- c(y[1],as.numeric(vars))
   		}
-  		if (any(y[-1]<=10^-5 && y[-1]>0) && type == 0) {
+  		if (any((y[-1] <= 10^-5) * (y[-1]>0)==1) && type == 0) {
   			lq <- sum(y[-1])
   			y[-1] <- y[-1] / lq
   			lq <- log(lq)
